2f1514fedc286a1930b9039d5fa06947b6e4a3da
JungleHornet <86211409+junglehornet@users.noreply.github.com>
2026-07-25 21:06:43 +0200

    new logos and colors and added footer

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ___
 <img src="https://media.longhornhacks.org/lhh-logo.jpeg" alt="Longhorn Hacks logo" style="width: 100px">
 
-### Visit the website at <a href="https://longhornhacks.org">longhornhacks.org</a>.
+### Visit the website at <a href="https://longhornhacks.org" target="_blank" rel="noopener noreferrer">longhornhacks.org</a>.
 
 This is the source code for the official website of Longhorn Hacks.
 The website uses SvelteKit and is hosted on Cloudflare Pages.
-To read the SvelteKit documentation, visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>.
+To read the SvelteKit documentation, visit <a href="https://svelte.dev/docs/kit" target="_blank" rel="noopener noreferrer">svelte.dev/docs/kit</a>.
 To contribute, make a pull request to the main branch of this repository.
 
 This repository is licensed under the MIT license. You can view the license in the file titled LICENSE
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import logo_blue_bg from '$lib/assets/logo-blue-bg.svg';
+    import logo_blazing_orange from '$lib/assets/logo-blazing-orange.svg';
 
 	let { children } = $props();
 </script>
@@ -10,7 +12,7 @@
  bg-dark-navy-blue text-bright-orange font-sans border-round
  rounded-b-xl items-center">
     <div class="mx-0 inline-block">
-        <a href="/" class="align-top"><img class="h-14.5 bg-bright-blue rounded-xl" src={favicon} alt="Longhorn Hacks Logo"></a>
+        <a href="/" class="align-top"><img class="h-14.5" src={logo_blazing_orange} alt="Longhorn Hacks White Logo"></a>
     </div>
     <div class="ml-auto mx-8 align-middle justify-right">
         <a href="/" class="mx-4 no-underline text-xl">Home</a>
@@ -20,6 +22,16 @@
         <a href="/events" class="mx-4 no-underline text-xl">Events</a>
     </div>
 </nav>
-<div class="bg-dark-navy-blue text-blazing-orange font-extralight font-sans h-screen">
+<div class="grow bg-background-grey text-text-grey font-extralight font-sans">
     {@render children()}
 </div>
+<footer class="flex text-burnt-orange bg-dark-navy-blue border-t-2 border-blazing-orange bottom-0 w-screen h-16 items-center">
+    <div class="flex grow items-center justify-items-start justify-start">
+        <a href="/" class=""><img class="h-12.5" src={logo_blue_bg} alt="Longhorn Hacks Logo"></a>
+        <a href="/legal" class="mx-4 no-underline text-lg align-middle">Legal</a>
+    </div>
+    <p class="mx-12 text-sm align-middle">
+        This website is Open Source:
+        <a href="https://github.com/longhornhacks/website" target="_blank" rel="noopener noreferrer">Source Code</a>
+    </p>
+</footer>
