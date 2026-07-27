2d5a470b69af418429c4957ddc6b1d9abd495045
JungleHornet <86211409+junglehornet@users.noreply.github.com>
2026-07-28 01:31:28 +0200

    new styling and added donate button in navbar

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,7 @@
 <svelte:head><title>Longhorn Hacks - About</title></svelte:head>
 
 <p class="text-4xl">About</p>
-<br>
-<p class="text-2xl">Our Mission:</p>
+<hr>
 <p class="text-sm">
     Longhorn Hacks is a registered 501(c)(3) nonprofit organization dedicated to expanding access to technology
     education and creating opportunities for underserved communities. Through corporate sponsorships and fundraising
--- src/routes/programs/+page.svelte
+++ src/routes/programs/+page.svelte
@@ -1,6 +1,12 @@
 <svelte:head><title>Longhorn Hacks - Programs</title></svelte:head>
 
 <p class="text-4xl">Programs</p>
+<hr>
 <p class="text-sm">
-    This is currently a placeholder page while the website is under construction. Try coming back later.
+    Longhorn Hacks is still a <i>very</i> young organization so we are yet to plan out our programs. However, we do
+    still have a general idea of what they will be: we are aiming to have programs that teach beginner programming and
+    AI use for programming, as well as more advanced classes, such as data structures or machine learning. We also hope
+    to be able to run workshops that are even more advanced where students get to create their own advanced projects,
+    such as their own programming language or AI chatbot. Thank you for sticking with us while we figure out what the
+    future holds for Longhorn Hacks!
 </p>
