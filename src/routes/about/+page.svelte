8f70f1fcf8e42c87480b2f6945049cba19ea6a07
JungleHornet <86211409+junglehornet@users.noreply.github.com>
2026-07-27 20:09:55 +0200

    restyled nav bar in style of frutiger aero

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,26 @@
+<script>
+</script>
+
 <p class="text-4xl">About</p>
+<br>
+<p class="text-2xl">Our Mission:</p>
 <p class="text-sm">
-    We're a registered non-profit dedicated to providing Computer Science and AI education to under-privileged and
-    under-funded communities across central texas.
+    Longhorn Hacks is a registered 501(c)(3) nonprofit organization dedicated to expanding access to technology
+    education and creating opportunities for underserved communities. Through corporate sponsorships and fundraising
+    events, we work to bridge the digital divide by increasing computer science and artificial intelligence literacy.
+    Our mission is to connect people through technology and empower the next generation of leaders in a rapidly
+    developing world.
+</p>
+<br>
+<p class="text-2xl">Leadership</p>
+<p class="text-lg">
+    Co-president: Varsha A.<br>
+    Co-president: Leslie C.<br>
+    Secretary: Vihaan K.<br>
+    Treasurer: Anishka K.<br>
+    Director of Curriculum: Aarjit (last name?)<br>
+    Director of Curriculum: Connor E.<br>
+    Director of Partnerships: Jovan N.<br>
+    Director of Partnerships: Namish J.<br>
+    Director of Marketing: Luana T.<br>
 </p>
